468f2b883c5c9e93795a122b98c8c567581311f6
Brian Leip <brianleip@gmail.com>
2019-09-13 00:31:34 -0700

    Completed week 2 all quizzes, challenges and assignments

---
+++ Week 2/Project - Hello-World/main.cpp
@@ -0,0 +1,35 @@
+/**
+ * @file main.cpp
+ * University of Illinois
+ * Object-Oriented Data Structures in C++ - Week 2
+ * Hello World Project
+ *
+ * @author Eric Huber - University of Illinois staff - July 2019
+**/
+
+#include <iostream>;
+using namespace std;
+
+int main() {
+    cout << "Hello, world!" << endl;
+    cout << "Greetings from Illinois!" << endl;
+    
+    return 0;
+}
+
+
+// Notes:
+
+// If you get a compiler error saying "undefined reference to main",
+// it means you haven't defined your main function correctly.
+
+// Standard output is the system stream for normal text output on the terminal.
+// One way to write to it, that you have learned, is with std::cout.
+
+// Your main function should write the following messages to standard output:
+// Hello, world!
+// Greetings from Illinois!
+
+// Also note, the autograder does not care about letter case, punctuation,
+// or spacing! But it does care about spelling and the order of words!
+
